c7b8415884090abb0b114a2522450d52b9bef7e9
Steven Cranham <Steven@codeplex.nl>
2013-05-13 11:37:19 +0200

    updated user controls to beloaded from dataset

--- server.r
+++ server.r
@@ -1,9 +1,59 @@
 library(shiny)
 
+loadData = function(){
+  source(file="lib/rename-columns.R")
+  
+  simulationData = read.csv("testdata/wormsimtotal.txt", sep=" ")
+  simulationData = renameWormdata(simulationData)
+}
+
 shinyServer(function(input, output) {
-	
+  data = loadData()
+  
+  output$treatmentIntervalUi <- renderUI({
+    checkboxGroupInput("treatmentInteval", "Treatment interval:", levels(data$treatment_interval))
+  })
+  
+  output$futureRoundsUi <- renderUI({
+    future_rounds = data$futurerounds
+    min_rounds = min(future_rounds)
+    max_rounds = max(future_rounds)
+    
+    sliderInput("future_rounds", "Future rounds:", 
+                min=min_rounds, max=max_rounds, value=max_rounds,step=1)
+  })
+  
+  output$pastRoundsUi <- renderUI({
+    past_rounds = data$pastrounds
+    min_rounds = min(past_rounds)
+    max_rounds = max(past_rounds)
+    
+    sliderInput("past_rounds", "Past rounds:", 
+                min=min_rounds, max=max_rounds,value=min_rounds,step=1)
+  })
+  
+  output$coverage <- renderUI({
+    selectInput("dataset", "Coverage:",
+                choices = levels(data$coverage))
+  })
+  
+  output$exposure <- renderUI({
+    selectInput("dataset", "Exposure:",
+                choices = levels(data$exposure))
+  })
+  
+  output$IVMSet <- renderUI({
+    selectInput("dataset", "IVMSet:",
+                choices = levels(data$IVMset))
+  })
+  
+  output$precontrol <- renderUI({
+    selectInput("dataset", "Precontrol:",
+                choices = levels(data$precontrol))
+  })
+  
   output$distPlot <- renderPlot({
-	  
-    plot(rnorm(input$future_rounds, 1, 2))
+    current = data$futurerounds == input$future_rounds
+    plot(1:100, 101:200)
 	})
 })
--- ui.r
+++ ui.r
@@ -7,20 +7,13 @@ shinyUI(pageWithSidebar(
   headerPanel("Onchocerciasis"),
   
   sidebarPanel(
-	  selectInput("dataset", "Treatment period:", 
-	                  choices = c("Annual", "Semi-Annual", "Quarterly")),
-					  
-	  sliderInput("future_rounds", "Future rounds:", 
-	                  min=0, max=20, value=20),
-
-	  sliderInput("past_rounds", "Past rounds:", 
-	                  min=0, max=20, value=0),
-					  
-	  selectInput("dataset", "Future coverage:", 
-	                  choices = paste(1:100, NULL, sep="%"), selected = "50%" ),
-					  
-	  selectInput("dataset", "Past coverage:", 
-	                  choices =paste(1:100, NULL, sep="%"), selected = "50%" )
+    uiOutput("treatmentIntervalUi"),
+    uiOutput("futureRoundsUi"),
+    uiOutput("pastRoundsUi"),
+    uiOutput("coverage"),
+    uiOutput("exposure"),
+    uiOutput("IVMSet"),
+    uiOutput("precontrol")
 	),
   
   mainPanel(
