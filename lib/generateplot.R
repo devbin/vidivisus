7d4f7299f67663e9c1e814bd17401c353f638794
Rutger <rutger@devbin.nl>
2013-05-31 22:18:25 +0200

    Added line indicators for x/y elimination intersection

--- lib/generateplot.R
+++ lib/generateplot.R
@@ -24,6 +24,16 @@ plotdata = function(data, input)
   plotdata = rbind(dataframe_pastrounds, dataframe_futurerounds)
 }
 
+
+get_elim_expectation_x = function(data, interval) {
+  sub = subset(data, data$treatment_interval == paste(c("Future", interval, "treatment"), collapse=' '))$x
+  
+  # max() cannot be called on an empty vector (NULL) or on a vector containing NA (returns NA)
+  # return max() on a vec with all NA's filtered out, otherwise return NA
+  sub = if (length(sub[!is.na(sub)]) > 0) max(sub, na.rm=T) else NA
+  sub
+}
+
 draw = function(data, input){
   
   plot = NA
@@ -35,15 +45,32 @@ draw = function(data, input){
     lim_x=max(finaldata$x)
     lim_y=max(finaldata$y)
     
-    begin_x = 2013 - max(finaldata$pastrounds)
-    end_x = 2013 + max(finaldata$futurerounds)
+    begin_x = 0 - max(finaldata$pastrounds)
+    end_x = 0 + max(finaldata$futurerounds)
     begin_y = 0
     end_y = 100
+    
+    line      = subset(finaldata, finaldata$elimination_probability %in% seq(0.988, 0.999, 0.0001))
+    annual    = get_elim_expectation_x(line, "annual")
+    semi      = get_elim_expectation_x(line, "semiannual")
+    quarterly = get_elim_expectation_x(line, "quarterly")
+    
+    elim_exp = c(annual, semi, quarterly)    
+    elim_exp_line = c("annual elimination", "semi elimination", "quarterly elimination") #c("annual", "semi", "quarterly")
+    vlines = data.frame(xint=elim_exp, grp=elim_exp_line)
+    vlines = vlines[!is.na(vlines$xint),] # not all scenario's reach P(elim) = 0.999 and cause warnings
+    
+    hlines = data.frame(yint=c(
+      lim_y/end_y * 99, 
+      lim_y/end_y * 95, 
+      lim_y/end_y * 90))
     
     plot = ggplot(finaldata, aes(x=x, y=y, colour=treatment_interval)) + geom_line() +
         scale_x_discrete(breaks=seq(0 , lim_x, 8), labels=seq(begin_x, end_x, 2)) +
         scale_y_discrete(breaks=seq(0 , lim_y, lim_y/end_y*5), labels=seq(begin_y, end_y, 5)) +
-        xlab("Years") + ylab("Elimination probability")
+        xlab("Years") + ylab("Elimination probability") + 
+        geom_hline(data=hlines, aes(yintercept=yint), colour="darkgray", linetype="dashed", size=0.4) +
+        geom_vline(data=vlines, aes(xintercept=xint, colour=grp), linetype="longdash", size=0.4)
   }
   plot
 }
